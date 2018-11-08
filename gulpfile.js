fc9e08cbd365e0553b1521a3e75b49a02d4100af
risakanagawa <gw.09ltry@gmail.com>
2018-11-07 19:18:49 -0800

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
-var gulp = require("gulp"); // Load Gulp!
-// Now that we've installed the uglify package we can require it:
-var uglify = require("gulp-uglify"),
+var gulp = require("gulp"); 
+var terser = require("gulp-terser"),
     rename = require("gulp-rename"),
     browserSync = require('browser-sync').create(),
     eslint = require('gulp-eslint'),
@@ -14,13 +13,13 @@ var uglify = require("gulp-uglify"),
 
 gulp.task("scripts", function () {
     return gulp
-        .src("./js/*.js") // What files do we want gulp to consume?
+        .src("./js/*.js") 
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(babel())
-        .pipe(uglify()) // Call the uglify function on these files
-        .pipe(rename({ extname: ".min.js" })) // Rename the uglified file
-        .pipe(gulp.dest("./build/js"));// Where do we put the result?
+        .pipe(terser()) 
+        .pipe(rename({ extname: ".min.js" }))
+        .pipe(gulp.dest("./build/js"));
 
 });
 
--- js/main.js
+++ js/main.js
@@ -1,64 +1,64 @@
 $(function () {
-    let url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+    let url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
     url += '?' + $.param({
-        'api-key': "c525f45dacf9413ca3c4cb50cdeff81f"
+        'api-key': 'c525f45dacf9413ca3c4cb50cdeff81f'
     });
-    const apiKey = "c525f45dacf9413ca3c4cb50cdeff81f";
+
+    $('.loading__img').show();
 
     $.ajax({
-        type: "GET",
-        dataType: "json",
+        type: 'GET',
+        dataType: 'json',
         url: url
     })
         .done(function (data) {
+            $('.loading__img').hide();
             const articleInfo = data.results;
-            console.log(articleInfo)
             //CREATE--FOR--OPTIONS
             const optionList = articleInfo.reduce((i, val) => (i.indexOf(val.section) !== -1) ? i : [...i, val.section], []);
-            $.each(optionList, function (j) {
-                $(".header__category").append('<option value=' + optionList[j].replace(" ", "") + '>' + optionList[j] + '</option>');
+            $.each(optionList, function (i) {
+                $('.header__category').append('<option value=' + optionList[i].replace(' ', '') + '>' + optionList[i] + '</option>');
             });
 
             //CHANGE--EVENT
-            $('[name="category"]').on("change", function () {
+            $('[name="category"]').on('change', function () {
 
                 let selectedOption = $(this).val();
-                $(".box").empty();
+                $('.article-section').empty();
 
                 // GET--FIRST--TWELVE--ARTICLES
-                let thisCategoryArticles = articleInfo.filter(function (article) {
-                    var thisSection = article.section.replace(" ", "");
-                    if (selectedOption == thisSection && article.multimedia.length !== 0) {
+                let CategoryArticles = articleInfo.filter(function (article) {
+                    let thisSection = article.section.replace(' ', '');
+                    if (selectedOption === thisSection && article.multimedia.length !== 0) {
                         return article;
                     }
                 });
-                let twelveArticles = thisCategoryArticles.slice(0, 13);
+                let twelveArticles = CategoryArticles.slice(0, 12);
 
 
-                $.each(twelveArticles, function (key) {
-
-
-                    var imageUrl = this.multimedia[4].url;
-                    var newsAbstract = this.abstract;
+                $.each(twelveArticles, function () {
+                    let imageUrl = this.multimedia[4].url;
+                    let newsAbstract = this.abstract;
 
                     //CREATE--ARTICLE--BOX
                     $('<div class="article_box">')
                         .css('background-image', 'url(' + imageUrl + ')')
                         .append('<p class="article__text">' + newsAbstract + '</p>')
-                        .appendTo(".box");
+                        .appendTo('.article-section');
 
                     // HEADER--MOVE--CSS
-                    $(".header")
-                        .css("height", "20vh")
-                        .css("transition", "1s");
-                    $(".header__logo").css("width", "15vh");
-
-
+                    $('.header')
+                        .css('height', '200px')
+                        .css('transition', '1s');
+                    $('.header__logo')
+                        .css('width', '15vh');
                 });
 
             });
         })
         .fail(function () {
-            $(".box").append("Sorry there was an error.");
+            $('.article-section').append('Sorry there was an error.')
+                                 .css('text-align', 'center');
         });
 });
+
